0099c186c5accd881fb4671d2a7e98336a849fe0
Surur Ahmed <hieliasahmed3@gmail.com>
2021-06-18 19:04:43 +0200

    initial assignment with only GetNumberOfTextAppearance method added.

---
+++ NameCounter/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace NameCounter
+{
+    class Program
+    {
+        public static string[] args;
+        Program(string[] args)
+        {
+            Program.args = args;
+        }
+        private void Run()
+        {
+            var f = File.Open(args[0], FileMode.Open);
+            int pos = args[0].IndexOf('.');
+            string name = args[0].Substring(0, pos);            
+            System.IO.StreamReader file = new System.IO.StreamReader(f);
+            string line;
+            //int counter = 0;
+            int counter = GetNumberOfTextAppearance(file, name); 
+            while (true)
+            {
+                line = file.ReadLine();
+                if (line == null) break; //What if the file continues after empty line
+                if (line.Contains(name)) //What if there are multiple number of the name in every line
+                    counter++;
+            }
+            Console.WriteLine("found " + counter);
+        }
+
+        private int GetNumberOfTextAppearance(StreamReader file, string name)
+        {
+            string line;
+            while (true)
+            {
+                line = file.ReadLine();
+                if (line == null) break; //What if the file continues after empty line
+                if (line.Contains(name)) //What if there are multiple number of the name in every line
+                    counter++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program(args);
+            program.Run();
+        }
+    }
+}
---
+++ readFile/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace readFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader(@"C:\Users\User\Desktop\Sample.txt");
+                  //first line
+                  line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    
+                    Console.WriteLine(line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                                  
+
+                                       
+
+                }
+                
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("could not find file " + e );
+            }
+            finally
+            {
+                Console.WriteLine("End of program");
+            }
+            int count;
+
+            StreamReader streamReader = File.OpenText(@"C:\Users\User\Desktop\Sample.txt");
+            using (StreamReader reader = streamReader)
+            {
+                //(.+?)(\.[^.] *$|$)
+                //
+                string contents = reader.ReadToEnd();
+               // var reg = new Regex(@"[^\s]+");
+                MatchCollection matches = Regex.Matches(contents, "Sample", RegexOptions.IgnoreCase);
+                count = matches.Count;
+                Console.WriteLine(" The word Sample" + " Found " + "{1}" + " Times on this file", contents, count);
+            }
+
+
+
+
+
+
+        }
+    }
+}
